c5a03a20aa36b219485d2802d3ea3b4d7c731b85
kingfoxxx <brightally6@gmail.com>
2023-09-16 17:18:27 -0700

    clone2

---
+++ models/amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This is the amenity class in file"""
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
+
+
+class Amenity(BaseModel, Base):
+    """This is the class for Amenities
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
