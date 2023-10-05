abbc3cc9bb375b5e8e5aa0376c5f2c43fbe16c0a
kingfoxxx <brightally6@gmail.com>
2023-10-05 15:07:07 +0000

    clone

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,18 +1,51 @@
-# Setup the web servers for the deployment of web_static
-exec { '/usr/bin/env apt -y update' : }
--> package { 'nginx':
-  ensure => installed,
-}
--> file { '/data':
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlw\u4? permanent;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
   ensure  => 'directory'
-}
--> file { '/data/web_static':
+} ->
+
+file { '/data/web_static':
   ensure => 'directory'
-}
--> file { '/data/web_static/releases':
+} ->
+
+file { '/data/web_static/releases':
   ensure => 'directory'
-}
--> file { '/data/web_static/releases/test':
+} ->
+
+file { '/data/web_static/releases/test':
   ensure => 'directory'
-}
--> file { '/data/web_static/shared':
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
+
